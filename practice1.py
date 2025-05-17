dff538bf59da68fd3477620b8fa06b595cef1679
Harshit4200 <harshitjain2466@gmail.com>
2025-05-17 19:13:20 +0530

    python problems

--- practice1.py
+++ practice1.py
@@ -1,1 +1,13 @@
-print("hello")
+# Q1
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+sum = num1+num2
+print("sum:",sum)
+# Q2
+remainder=num1%num2
+print("remainder:",remainder)
+# Q3
+print((num1+num2)/2)
+# Q4
+square=num2*num2
+print(square)
